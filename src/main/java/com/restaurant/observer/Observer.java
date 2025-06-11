4b55e0ebf78f4faa92f4d16fd315f81a33a6e580
mor19 <86059798+mor19@users.noreply.github.com>
2025-06-12 01:11:47 +0200

    Add missing updates
    forgot to push updated Subject and Observer class

--- src/main/java/com/restaurant/observer/Observer.java
+++ src/main/java/com/restaurant/observer/Observer.java
@@ -2,6 +2,9 @@ package com.restaurant.observer;
 
 import java.util.List;
 
+import com.restaurant.Customer;
+
 public interface Observer {
-    void update(String currentProgressText, List<String> customerQueue, int currentStep);
+
+    void update(String currentProgressText, List<Customer> customerQueue, int currentStep);
 }
